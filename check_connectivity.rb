eeb0c7ebe4896af48055bae337ec6c85b89a36da
azet <azet@azet.org>
2012-01-26 14:06:47 +0100

    intendation

--- check_connectivity.rb
+++ check_connectivity.rb
@@ -9,16 +9,16 @@ require 'io/handler'
 require 'ping'
 
 def check_connectivity(host, service)
-	begin
-		ping = Ping.pingecho(host, 4, service)
-		if ping
-			puts $ret_line + 'PING check' + ' - OK'.col_status
-		else
-			puts $ret_line + 'PING check ' + host + ' - FAILURE'.col_red
-			puts $ret_line + '..tracerouting..'.col_red
-			traceroute=`which traceroute`.chomp
-			exec=`#{traceroute} -n #{host}`
-			err('connectivity problem!', exec)
-    		end
-	end
+    begin
+        ping = Ping.pingecho(host, 4, service)
+        if ping
+            puts $ret_line + 'PING check' + ' - OK'.col_status
+        else
+            puts $ret_line + 'PING check ' + host + ' - FAILURE'.col_red
+            puts $ret_line + '..tracerouting..'.col_red
+            traceroute=`which traceroute`.chomp
+            exec=`#{traceroute} -n #{host}`
+            err('connectivity problem!', exec)
+            end
+    end
 end
