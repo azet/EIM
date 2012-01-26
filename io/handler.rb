3a8e8834fd67206dcfbea69d35ca15e99a1e8efd
azet <azet@azet.org>
2012-01-26 14:11:40 +0100

    minor change

--- io/handler.rb
+++ io/handler.rb
@@ -6,11 +6,11 @@
 #
 ########################################################################
 class String
-  def col_red; colorize(self, "\e[31m"); end
-  def col_red_bg; colorize(self, "\e[30m\e[41m"); end
-  def col_blue; colorize(self, "\e[1m\e[33m\e[44m"); end
-  def col_status; colorize(self, "\e[32m"); end
-  def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+    def col_red; colorize(self, "\e[31m"); end
+    def col_red_bg; colorize(self, "\e[30m\e[41m"); end
+    def col_blue; colorize(self, "\e[1m\e[33m\e[44m"); end
+    def col_status; colorize(self, "\e[32m"); end
+    def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
 end
 $ret_line = 'EIM'.col_status + ' => '
 
@@ -19,7 +19,7 @@ def err(e, param)
     require 'io/alerting'
     return puts $ret_line + e, $0.col_status + ' => ' + 'error: '.col_red + e.to_s.col_red_bg
 ensure
-     puts $ret_line + 'alerting'.col_red_bg
-    #send_sms(e.to_s, param.to_s)
-    #send_mail(e.to_s, param.to_s)
+    puts $ret_line + 'alerting'.col_red_bg
+    send_sms(e.to_s, param.to_s)
+    send_mail(e.to_s, param.to_s)
 end
