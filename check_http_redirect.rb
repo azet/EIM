5a187d269ed6ba7dca321ea59df49acd81a23565
azet <azet@azet.org>
2012-01-27 14:39:14 +0100

    redirect checking!

--- check_http_redirect.rb
+++ check_http_redirect.rb
@@ -12,8 +12,8 @@ def check_http_redirect(url)
     begin
         basic_auth = url.split('@').first.split('//').last.split(':') if url =~ /@/
         url =~ /@/ ? match = open(url, :http_basic_authentication=>[basic_auth[0], basic_auth[1]]).read : match = open(url).read
-        
-        if match.empty? or !match =~ /refresh/
+
+        if match.empty? or !match =~ /http-equiv=\"refresh\"/
             puts $ret_line + "REDIRECT check '#{url}' - FAILED".col_red
         else
             puts $ret_line + "REDIRECT check" + " - OK".col_status
--- dispatch.rb
+++ dispatch.rb
@@ -2,7 +2,7 @@
 ########################################################################
 #
 #  developer:    aaron.zauner@emarsys.com
-#  descr_self:   Monitoring check dispatcher
+#  descr_self:   Monitoring check Dispatcher
 #
 ########################################################################
 require 'io/handler'
@@ -27,12 +27,19 @@ begin
                     require 'check_http_url'
                     check_http_url(object[1])
                 }
+            when 'check_http_redirect'
+                puts $ret_line + '>> - checking redirect of:'.col_status
+                puts $ret_line + name + ' -- ' + object[1]
+                threads << Thread.new {
+                    require 'check_http_redirect'
+                    check_http_redirect(object[1])
+                }
             when 'check_connectivity'
                 puts $ret_line + '>> - checking connectivity of:'.col_status
                 puts $ret_line + name + ' -- ' + object[1] + ':' + object[2]
                 threads << Thread.new {
                     require 'check_connectivity'
-                      check_connectivity(object[1], object[2])
+                    check_connectivity(object[1], object[2])
                 }
             else
                 puts $ret_line + 'error: '.col_red + object[0].col_red_bg + ' <-- sorry, i dont know what to do with this..'.col_red
